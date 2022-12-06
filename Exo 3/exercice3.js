f3243474071a2d6678f83627a09ecd4f321c7f9b
victorien <deadvicmangame@gmail.com>
2022-12-06 10:15:46 +0100

    exo 85 les objets

--- Exo 3/exercice3.js
+++ Exo 3/exercice3.js
@@ -1,13 +1,17 @@
 let personne = {
-    "prenom" : "James",
-    "nom" : "Bond",
-           :    ,
-            :
+    prenom : "James",
+    nom : "Bond",
+    nomDeCode : "007",
+    metier : "agent secret"
 };
 
 /* Afficher ci dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
  <propriété nom de notre objet>, agent <propriété nomDeCode de notre objet>" dans un alert */
 
-alert();
+alert(personne.nom);
+alert(personne.prenom);
+alert(personne.nomDeCode);
+alert(personne.metier);
+
 
 
